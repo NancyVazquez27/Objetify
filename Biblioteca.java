e99ec8e5b6a92faf0368149babf5d708bdf75023
Nancy <31119437+NancyVazquez27@users.noreply.github.com>
2017-08-31 23:56:41 -0500

    Se agrego la domcumentacion necesaria.

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
-
+/**
+ * La clase biblioteca fue creada para simular que los archivos
+ * de musica se guardaran aqui.
+ * Su constructor no recibe parametros.
+ * 
+ * @author Nancy Vazquez
+ * @version 08.31.17
+ */
 public class Biblioteca
 {
     private ArrayList<String> canciones;
@@ -10,12 +17,22 @@ public class Biblioteca
         canciones = new ArrayList<String>();
         reproductor = new Reproductor();
     }
-    
+    /**
+     * El metodo agregarCancion permite agregar un archivo ala biblioteca.
+     * @param recibe la varible nombreArchivo.
+     * no retorna nada.
+     */
     public void agregarCancion(String nombreArchivo)
     {
         this.canciones.add(nombreArchivo);
     }
     
+    /**
+     * El metodo reproducir permite recibir la posicion en la que se
+     * encuentra en el arreglo de listas  la cancion que se desea reproducir
+     * @param numCancion
+     * 
+     */
     public void reproducir(int numCancion)
     {
         reproductor.reproducir(canciones.get(numCancion));
--- Reproductor.java
+++ Reproductor.java
@@ -1,3 +1,8 @@
+/**
+ * La clase reproductor se crea para simular el reproductor de canciones.
+ * @param nombreArchivo ( nombre de la cancion que se desea reproducir)
+ * No retorna nada.
+ */
 public class Reproductor
 {
     public void reproducir(String nombreArchivo)
